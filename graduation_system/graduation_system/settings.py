48138a1df1dfb8039dcb7c0074f2fa8f7573fdeb
Amirmohsen Ahanchi <ahanchi@gmail.com>
2018-02-04 16:48:06 +0330

    static files (bootstrap...) and base html added

--- graduation_system/graduation_system/settings.py
+++ graduation_system/graduation_system/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
---
+++ graduation_system/main/urls.py
@@ -0,0 +1,9 @@
+# Amirmohsen Ahanchi
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
