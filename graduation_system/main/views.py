af4fd7862b6cb83ecf0fd461dd5786a9f9b3cdc2
amirhoseinsa <amirsfr53@gmail.com>
2018-02-05 14:49:24 +0330

    Nothing

--- graduation_system/main/views.py
+++ graduation_system/main/views.py
@@ -1,8 +1,16 @@
+
 from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
-from main.models import Process
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http.response import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404, redirect
 
+# Create your views here.
+from django.urls import reverse
+
+from main.models import ProcessForm
+from main.models import Process
 
 def index(request):
     return render(request, 'main/base.html')
@@ -12,6 +20,10 @@ def designer_view(request):
     processes = Process.objects.all()
     return render(request, 'main/designer.html', {'processes': processes})
 
+
 def process_view(request, process_id):
     process = get_object_or_404(Process, id=process_id)
     return render(request, 'main/process.html', {'process': process})
+
+
+
