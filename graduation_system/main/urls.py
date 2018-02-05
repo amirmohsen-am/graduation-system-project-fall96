e518bc8cba50584f0e294774a788242dea7ee156
Amirmohsen Ahanchi <ahanchi@gmail.com>
2018-02-05 16:50:37 +0330

    add logout

--- graduation_system/main/urls.py
+++ graduation_system/main/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # url(r'^login/$', auth_views.login, name='login'),
     url(r'^login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^designer$', views.designer_view, name='designer-view'),
-    url(r'^process/(?P<process_id>\d+)$', views.process_view, name='process-view')
+    url(r'^process/(?P<process_id>\d+)$', views.process_view, name='process-view'),
+    url(r'^task/(?P<task_id>\d+)$', views.task_view, name='task-view'),
 ]
--- graduation_system/main/views.py
+++ graduation_system/main/views.py
@@ -10,17 +10,19 @@ def index(request):
     return render(request, 'main/base.html')
 
 
-@login_required
+@login_required(login_url='/login/')
 def designer_view(request):
     processes = Process.objects.all()
     return render(request, 'main/designer.html', {'processes': processes})
 
-@login_required
+
+@login_required(login_url='/login/')
 def process_view(request, process_id):
     process = get_object_or_404(Process, id=process_id)
     return render(request, 'main/process.html', {'process': process})
 
-@login_required
+
+@login_required(login_url='/login/')
 def task_view(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    return render(request, 'main/task.html')
+    return render(request, 'main/task.html', {'task': task})
