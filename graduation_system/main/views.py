eb351c555259bcda8e7136fee7c8553ced092a6a
Amirmohsen Ahanchi <ahanchi@gmail.com>
2018-02-05 18:39:36 +0330

    task_add view

--- graduation_system/main/views.py
+++ graduation_system/main/views.py
@@ -25,13 +25,45 @@ def designer_view(request):
 @login_required(login_url='/login/')
 def process_view(request, process_id):
     process = get_object_or_404(Process, id=process_id)
-    form = ProcessForm(instance=process)
+    if request.method == 'POST':
+        form = ProcessForm(request.POST, instance=process)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Process was successfully edited')
+            return redirect(reverse('process-view'))
+    else:
+        form = ProcessForm(instance=process)
+
     return render(request, 'main/process.html', {'process': process, 'form': form})
 
 
 @login_required(login_url='/login/')
 def task_view(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Task was successfully edited')
+            return redirect(reverse('task-view'))
+    else:
+        form = TaskForm(instance=task)
+
     return render(request, 'main/task.html', {'task': task, 'form': form})
 
+
+@login_required(login_url='/login/')
+def task_add(request, process_id):
+    process = get_object_or_404(Process, id=process_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Task has been created')
+            return redirect(reverse('task-view'))
+    else:
+        form = TaskForm()
+    form.process = process
+    form.fields["process"].initial = process
+    form.fields["process"].disabled = True
+    return render(request, 'main/task_add.html', {'form': form})
