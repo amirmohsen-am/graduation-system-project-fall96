7f5ba46d0b7d77a8b54329fedd9c46d5f4ed3504
Amirmohsen Ahanchi <ahanchi@gmail.com>
2018-02-05 15:49:38 +0330

    show user and model changes

--- graduation_system/main/urls.py
+++ graduation_system/main/urls.py
@@ -7,7 +7,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, name='login'),
+    # url(r'^login/$', auth_views.login, name='login'),
+    url(r'^login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     url(r'^designer$', views.designer_view, name='designer-view'),
     url(r'^process/(?P<process_id>\d+)$', views.process_view, name='process-view')
 ]
--- graduation_system/main/views.py
+++ graduation_system/main/views.py
@@ -1,17 +1,26 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from main.models import Process
+from main.models import Process, Task
 
 
+@login_required(login_url='/login/')
 def index(request):
     return render(request, 'main/base.html')
 
 
+@login_required
 def designer_view(request):
     processes = Process.objects.all()
     return render(request, 'main/designer.html', {'processes': processes})
 
+@login_required
 def process_view(request, process_id):
     process = get_object_or_404(Process, id=process_id)
     return render(request, 'main/process.html', {'process': process})
+
+@login_required
+def task_view(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'main/task.html')
